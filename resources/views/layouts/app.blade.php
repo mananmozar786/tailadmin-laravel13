9f3703c3f7f860a511942d8eed1a7250d8eae05c
mananmozar786 <mananmozar786@gmail.com>
2026-04-05 16:01:23 +0530

    implement main and fullscreen layout templates with Alpine.js theme and sidebar state management

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Alpine.js -->
-    {{-- <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
+    {{-- Alpine initialized via resources/js/app.js --}}
 
     <!-- Theme Store -->
     <script>
